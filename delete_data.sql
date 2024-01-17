ea9a4679d20ce1d0193132292fde9af6a604dd17
Hamiyawati <hamiyawati@gmail.com>
2024-01-17 13:10:43 +0700

    format edited

--- delete_data.sql
+++ delete_data.sql
@@ -5,7 +5,7 @@ use online_retail_db;
 -- delete products that have not been sold
 delete from products
 where not exists(
-select 1
-from purchase_detail
-where purchase_detail.product_id = products.product_id
+    select 1
+    from purchase_detail
+    where purchase_detail.product_id = products.product_id
 );
